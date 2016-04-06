c444a35abeee56dc7683adfcfb038fbd647477ff
曾庆龙 <zengqinglong@MacBook-Pro-2.local>
2016-04-06 18:17:56 +0800

    s

--- ad_init_mysql.c
+++ ad_init_mysql.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <mysql/mysql.h>
+#include "ad.h"
+#include "list.h"
 
 
 
 int g_max_prio= 10;
+
+ad_struct_t  ad_inuse[10][200];
+
+
 
 int main()
 {
@@ -26,10 +32,14 @@ int main()
   } 
 
 
-
+	
+  int index=0;
+  ad_struct_t * struct_ptr = NULL;
   for(i=0; i<g_max_prio; i++)
   {
-     snprintf(query, 1024, "select * from ad where prio=%d\n", i);	
+  	 index = 0;
+     snprintf(query, 1024, "select * from ad where prio=%d\n", i);
+	 
      if(mysql_query(&mysql, query) == 0)
 	{
 		
@@ -41,7 +51,10 @@ int main()
 
 
     	while (row = mysql_fetch_row(result)) {
-      		printf("row name %s", row[0]);			
+			
+			struct_ptr = &(ad_inuse[i][index]);
+			struct_ptr->id = atoi(row[0]);
+						
     	}
 	}
 	else
